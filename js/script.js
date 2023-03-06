1ef4af3efce98f13cfee14f0eaba9c0d5c32575d
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-06 14:21:58 +0600

    practise is done

---
+++ js/script.js
@@ -0,0 +1,28 @@
+document.getElementById("btn1").addEventListener("click",function(){
+
+
+const get=document.getElementById("input1")
+const get2=get.value
+const parts2=parseInt(get2)
+
+const get6=document.getElementById("input2")
+const get3=get6.value
+const parts=parseInt(get3)
+
+
+const get4=document.getElementById("input3")
+const get5=get4.value
+const parts3=parseInt(get5)
+
+const total=parts+parts2+parts3
+
+
+
+let get44=document.getElementById("totalcalcu")
+
+get44.innerText=total
+
+
+
+
+})
